79a21f52c43627cde99005ad96067842b12db198
Mysticpasta1 <ethana5tc@outlook.com>
2023-06-17 23:53:26 -0500

    start of threading mob ais

---
+++ src/main/java/com/mystic/threadai/ThreadAI.java
@@ -0,0 +1,13 @@
+package com.mystic.threadai;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ThreadAI.MOD_ID)
+public class ThreadAI {
+    public static final String MOD_ID = "threadai";
+
+    @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) ();
+    }
+}
---
+++ src/main/java/com/mystic/threadai/mixin/EntityMixin.java
@@ -0,0 +1,11 @@
+package com.mystic.threadai.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+
+}
